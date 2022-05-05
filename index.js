b11a95b3b9167c6ec157243e027c3d233cf18661
sandy <sandy.b@appwrk.com>
2022-05-05 19:19:50 +0530

    updated code

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// console.log("This is the Indian Railways JS Page!");
 let id = "";
 window.addEventListener('load', function () {
     let formInput = document.querySelectorAll('.form-control');
@@ -38,17 +37,55 @@ function validation() {
     if (inputCategory == '') {
         document.getElementById("messageCategory").innerHTML = 'Please Select a Category!';
     }
-    else{
-
+    else if(inputFrom == inputTo){
+        document.getElementById("messageInputTo").innerHTML = 'Start Station and Destination cannot be same!';
+    }
+    else {
+        setData();
     }
 }
 
 function setData() {
-
+    let getFromInput = document.getElementById('from').value;
+    let getDateInput = document.getElementById('inputDate').value;
+    let getToInput = document.getElementById("to").value;
+    let getClassInput = document.getElementById('selectAllClasses').value;
+    let getCategoryInput = document.getElementById('selectCategory').value;
+    let passenger_records = [];
+    passenger_records = JSON.parse(localStorage.getItem("passengers")) ? JSON.parse(localStorage.getItem("passengers")) : []
+    passenger_records.unshift({
+        'from': getFromInput.trim(),
+        'date': getDateInput.trim(),
+        'to': getToInput,
+        'class': getClassInput,
+        'category': getCategoryInput
+    })
+    passenger_records = localStorage.setItem("passengers", JSON.stringify(passenger_records));
+    window.location.href = 'table.html';
+    document.getElementById("reset").click();
 }
 
 function swap() {
     let temp = document.getElementById("from").value;
     document.getElementById("from").value = document.getElementById("to").value;
     document.getElementById("to").value = temp;
+}
+
+function checkDate() {
+    //     let today = new Date(),
+    //     day = today.getDate(),
+    //     month = today.getMonth()+1,
+    //     year = today.getFullYear();
+    //          if(day<10){
+    //                 day='0'+day
+    //             } 
+    //         if(month<10){
+    //             month='0'+month
+    //         }
+    //         today = year+'-'+month+'-'+day;
+
+    // document.getElementById("inputDate").setAttribute("min", today);
+    // document.getElementById("inputDate").setAttribute("value", today);
+    document.getElementById('inputDate').setAttribute('min', new Date().toISOString().split('T')[0])
+
 }
--- table.js
+++ table.js
@@ -1,1 +1,41 @@
-// console.log("This is the Table Page");
+window.addEventListener('load', function (){
+    displayData();
+});
+
+function displayData() {
+    let getData = JSON.parse(localStorage.getItem("passengers"));
+    if (getData != null) {
+        let html = '';
+        let sno = 1;
+        for (let k in getData) {
+            html +=
+            `<tr>
+                <td>${sno}</td>
+                <td>${getData[k].from}</td>
+                <td>${getData[k].to}</td>
+                <td>${getData[k].date}</td>
+                <td>${getData[k].class}</td>
+                <td>${getData[k].category}</td>
+            </tr>`
+            sno++;
+        }
+        document.getElementById("tableBody").innerHTML = html;
+    }
+}
+
+function search(){
+    let filter = document.getElementById('search').value.toUpperCase();
+    let getTableData = document.getElementById('tableBody');
+    let tableRow = getTableData.getElementsByTagName('tr');
+    for(let i = 0; i < tableRow.length; i++){
+        let td = tableRow[i].getElementsByTagName('td')[1];
+        if(td){
+            let textValue = td.textContent || td.innerHTML;
+            if(textValue.toUpperCase().indexOf(filter) > -1){
+                tableRow[i].style.display = '';
+            }else{
+                tableRow[i].style.display = "none";
+            }
+        }
+    }
+}
